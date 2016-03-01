3b92981760ca4e6872b2d799a8a8df137642a96e
Sean Henry <sean.henry@moonpig.com>
2016-03-01 09:44:42 +0000

    Refactors common test code into single file

---
+++ tests/code_generator/code_generator_test_helper.rb
@@ -0,0 +1,45 @@
+class CodeGeneratorTestHelper
+
+	def file_is_generated_by_keys_values(generator, file_name, keys, values)
+		generated_code = generator.generate(keys, values).split("\n")
+		expected_file = file_lines file_name
+		if expected_file.size != generated_code.size 
+			handle_unequal_line_counts(expected_file, generated_code)
+			return false
+		end
+		i = 0
+		while i < expected_file.count do
+			if expected_file[i] != generated_code[i]
+				handle_unequal_lines_at(expected_file, generated_code, i)
+				return false
+			end
+			i += 1
+		end
+		true
+	end
+
+private
+	def file_lines(file_name)
+		file_path = Dir.pwd + "/tests/code_generator/" + file_name
+		File.readlines(file_path).map do |line|
+			line.gsub(/\n/, "")
+		end
+	end
+
+	def handle_unequal_line_counts(expected_file, generated_code)
+		puts "Expected line count #{expected_file.size}, got #{generated_code.size}"
+		puts "Generated lines:\n" + print_lines(generated_code)
+		puts "Expected lines: \n" + print_lines(expected_file)
+	end
+
+	def handle_unequal_lines_at(expected_file, generated_code, index)
+		puts "Expected line #{expected_file[index]} got: #{generated_code[index]}"
+		puts "Line number: #{index + 1}"
+		puts "Generated lines:\n" + print_lines(generated_code)
+		puts "Expected lines: \n" + print_lines(expected_file)
+	end
+
+	def print_lines lines 
+		lines.join("\n")
+	end
+end
--- tests/code_generator/objc_header_strings_generator_tests.rb
+++ tests/code_generator/objc_header_strings_generator_tests.rb
@@ -1,5 +1,6 @@
 require 'minitest/autorun'
 require './lib/code_generator/objc_header_strings_generator'
+require './tests/code_generator/code_generator_test_helper'
 
 class ObjcHeaderStringsGeneratorTests < Minitest::Test
 
@@ -10,57 +11,14 @@ class ObjcHeaderStringsGeneratorTests < Minitest::Test
 	end
 
 	def test_generate_generates_empty_enum_when_no_keys_or_values
-		assert_true file_is_generated_by_keys_values("empty_enum.h", [], [])
+		assert_file_is_generated_by_keys_values("empty_enum.h", [], [])
 	end
 
 	def test_generate_generates_many_cases_when_keys_and_values
-		assert_true file_is_generated_by_keys_values("two_case_enum.h", ["Key1", "Key2"], ["value1", "value2"])
+		assert_file_is_generated_by_keys_values("two_case_enum.h", ["Key1", "Key2"], ["value1", "value2"])
 	end
 
-	def file_is_generated_by_keys_values(file_name, keys, values)
-		ObjcHeaderStringsGenerator.new.generate [""], [""]
-		generated_code = ObjcHeaderStringsGenerator.new.generate(keys, values).split("\n")
-		expected_file = file_lines file_name
-		if expected_file.size != generated_code.size 
-			handle_unequal_line_counts(expected_file, generated_code)
-			return false
-		end
-		i = 0
-		while i < expected_file.count do
-			if expected_file[i] != generated_code[i]
-				handle_unequal_lines_at(expected_file, generated_code, i)
-				return false
-			end
-			i += 1
-		end
-		true
-	end
-
-	def file_lines(file_name)
-		file_path = Dir.pwd + "/tests/code_generator/" + file_name
-		File.readlines(file_path).map do |line|
-			line.gsub(/\n/, "")
-		end
-	end
-
-	def handle_unequal_line_counts(expected_file, generated_code)
-		puts "Expected line count #{expected_file.size}, got #{generated_code.size}"
-		puts "Generated lines:\n" + print_lines(generated_code)
-		puts "Expected lines: \n" + print_lines(expected_file)
-	end
-
-	def handle_unequal_lines_at(expected_file, generated_code, index)
-		puts "Expected line #{expected_file[index]} got: #{generated_code[index]}"
-		puts "Line number: #{index + 1}"
-		puts "Generated lines:\n" + print_lines(generated_code)
-		puts "Expected lines: \n" + print_lines(expected_file)
-	end
-
-	def print_lines lines 
-		lines.join("\n")
-	end
-
-	def assert_true(condition)
-		assert_equal(true, condition)
+	def assert_file_is_generated_by_keys_values(file_name, keys, values)
+		assert_equal(true, CodeGeneratorTestHelper.new.file_is_generated_by_keys_values(ObjcHeaderStringsGenerator.new, file_name, keys, values))
 	end
 end
