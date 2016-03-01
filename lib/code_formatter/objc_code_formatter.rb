b71ea4f63769febccea9476c10f1260a0218c4fd
Sean Henry <sean.henry@moonpig.com>
2016-03-01 12:05:55 +0000

    Adds code formatters

---
+++ lib/code_formatter/objc_code_formatter.rb
@@ -0,0 +1,9 @@
+require_relative 'code_formatter'
+
+class ObjcCodeFormatter < CodeFormatter
+
+	def format_string(string)
+		string.sub(/^[a-z]/, &:upcase)
+		.gsub(/_[A-Z]+/) { |s| downcase_string_unless_acronym(s) }
+	end
+end
