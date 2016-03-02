df4f0cf2d4c2f59d3d70cc18ddf553ae40407289
Sean Henry <sean.henry@moonpig.com>
2016-03-02 12:24:56 +0000

    Increments version number

--- test/rescodegen_tests.rb
+++ test/rescodegen_tests.rb
@@ -5,4 +5,8 @@ class RescodegenTest < Minitest::Test
     def test_that_it_has_a_version_number
         refute_nil ::Rescodegen::VERSION
     end
+
+    def test_version
+        assert_equal ::Rescodegen::VERSION, "0.1.2"
+    end
 end
